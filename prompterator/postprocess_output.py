109cc9618c5777dd6671205f11474115b1d102af
samsucik <ssucik@cisco.com>
2024-04-18 09:38:48 +0200

    Make the postprocessor do nothing by default

--- prompterator/postprocess_output.py
+++ prompterator/postprocess_output.py
@@ -2,4 +2,4 @@ def postprocess(generated_text: str) -> str:
     """
     Rewrite this function to postprocess the generated texts one by one.
     """
-    return generated_text[::-1]
+    return generated_text
